1a2f9650988500fae8c0d53870734d87e2fdece9
dlee3427 <david.lee.sl2634@yale.edu>
2020-06-17 16:22:22 -0400

    final

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -7,19 +7,45 @@ class TenantsController < ApplicationController
     erb :index 
   end 
 
-  get '/tenants/:id' do
+  get '/tenants/new' do
+    @apartments = Apartment.all 
+    erb :new 
+  end 
+
+  get '/tenants/:id/edit' do
     @tenant = Tenant.find(params[:id])
+    @apartments = Apartment.all
     erb :edit 
   end
+
+  get '/tenants/:id' do 
+    @tenant = Tenant.find(params[:id])
+    erb :show
+  end 
+
 
   patch '/tenants/:id' do
     tenant = Tenant.find(params[:id])
     tenant.update(name: params[:name])
-    tenant.update(apartment_id: params[:apartment_id])
-    redirect "/tenants/#{tenant.id}"
+    # binding.pry
+    tenant.update(apartment_id: params[:tenant][:apartment_id])
+    redirect "/tenants"
   end 
 
-  
 
+  delete '/tenants/:id' do 
+    Tenant.delete(params[:id])
+    redirect '/tenants'
+  end 
+
+  post '/tenants' do
+    tenant = Tenant.new(name: params[:name])
+    # tenant.update(name: params[:name])
+
+    tenant.apartment_id=(params[:tenant][:apartment_id])
+    tenant.save
+    
+    redirect '/tenants'
+  end 
 
 end
